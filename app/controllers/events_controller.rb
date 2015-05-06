439ba847619690c439c0e86dcdb1e917a543f865
Denise McCoy <Denise.L.McCoy@gmail.com>
2015-05-06 11:02:57 -0600

    setupModel

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,10 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
   end
 
   def import
+    Event.import(params[:file])
+    redirect_to root_url, notice: "Events imported."
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,17 @@
 class Event < ActiveRecord::Base
+  require 'csv'
+
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+
+      event_hash = row.to_hash # exclude the price field
+      event = Event.where(id: event_hash["id"])
+
+      if event.count == 1
+        event.first.update_attributes(event_hash)
+      else
+        Event.create!(event_hash)
+      end
+    end
+  end
 end
